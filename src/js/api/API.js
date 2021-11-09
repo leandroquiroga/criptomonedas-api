50ee25d9b6d4245e0c3e3a472e6c892e31a2ecf3
Leandro <leandroquiroga9514@gmail.com>
2021-11-09 16:40:18 -0300

    Update of the fetch by Try...Catch

--- src/js/api/API.js
+++ src/js/api/API.js
@@ -1,28 +1,35 @@
 import { ui } from "../module/module";
 import { selectCryptocoins } from './../module/module';
-
+import 'regenerator-runtime/runtime'
+import { async } from "regenerator-runtime";
 
 
 // show result of the consult
-export const consultAPI = (objCrypto) => {
+export const consultAPI =  async (objCrypto) => {
     const { moneda, crypto } = objCrypto;
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${crypto}&tsyms=${moneda}`
-    fetch(url)
-        .then(response => response.json())
-        .then(result => {
-            let price = result.DISPLAY[crypto][moneda];
-            ui.showPrice(price)
-        })
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let price = data.DISPLAY[crypto][moneda]
+        ui.showPrice(price)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // new Promise
 const createCrypto = (cryptos) => new Promise(resolve => resolve(cryptos))
-    
+
 // create the array of API with the 10 best coins
-export const cryptoAPI = () => {
+export const cryptoAPI = async () => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
-    fetch(url)
-        .then(response => response.json())
-        .then(result => createCrypto(result.Data))
-        .then(cryptos => selectCryptocoins(cryptos))
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        const crypto = await createCrypto(result.Data);
+        selectCryptocoins(crypto)
+    } catch (error) {
+        console.log(error)
+    }
 }
